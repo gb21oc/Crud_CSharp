afe692d79aad956950a7d5e4b3732bb0574d087c
Gabriel Jose <gabriel.soares@arphoenix.com.br>
2021-04-23 17:41:16 -0300

    Create Connection

--- crud/Properties/ConexaoDb.cs
+++ crud/Properties/ConexaoDb.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace crud.Properties
 {
@@ -13,7 +16,64 @@ namespace crud.Properties
 
         public ConexaoDb()
         {
-            cn = new SqlConnection();A
+            cn = new SqlConnection(ConfigurationManager.ConnectionStrings[@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=master;Data Source=WKT-0065D\SQLEXPRESS"].ToString());
+            cn.Open();
+        }
+
+        public void FecharConexao()
+        {
+            cn.Close();
+            cn.Dispose();
+        }
+
+        public DataTable DataTable_ConsultarDados(string sql)
+        {
+            DataTable td = new DataTable();
+            //Executa a consulta no db
+            try
+            {
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                SqlCommand command = new SqlCommand();
+                command.CommandTimeout = 50000;
+                dataAdapter.SelectCommand = command;
+
+                //preenche o dataTable
+                dataAdapter.Fill(td);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            return td;
+        }
+
+        public SqlDataReader Reader_ConsultarDados()
+        {
+                //Executa a query
+                SqlCommand command = new SqlCommand();
+                command.CommandTimeout = 50000;
+                SqlDataReader dataReader;
+                dataReader = command.ExecuteReader();
+                return dataReader;
+        }
+
+        public void ExecutaQuery(string sql)
+        {
+            SqlTransaction sqlTransaction = cn.BeginTransaction();
+            try
+            {
+                //Comando para a execução
+                SqlCommand command = new SqlCommand();
+                command.CommandTimeout = 50000;
+                command.Transaction = sqlTransaction;
+                command.ExecuteNonQuery();
+                sqlTransaction.Commit();
+            }
+            catch (SqlException ex)
+            {
+                sqlTransaction.Rollback();
+                MessageBox.Show(ex.ToString());
+            }
         }
 
     }
